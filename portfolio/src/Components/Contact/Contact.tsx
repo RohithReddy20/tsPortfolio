d5c270960cc220f820a20482b88968be63dea0e1
Rohith Reddy <74014434+RohithReddy20@users.noreply.github.com>
2022-12-02 07:58:54 +0000

    contact

---
+++ portfolio/src/Components/Contact/Contact.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import styles from "./Contact.module.css";
+import LineNavigator from "../LineNavigator/LineNavigator.js"
+
+// export interface IContactProps {
+// }
+// props: IContactProps
+
+export default function Contact () {
+  return (
+    <div className={styles.contact}>
+        <div className={styles.contactForm}>
+            Contact
+        </div>
+        <div className={styles.navigator}>
+                <LineNavigator />
+                
+            </div>
+    </div>
+  );
+}
